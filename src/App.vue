e433dadf1cc4cd6e6a1dbeabd91bf091b586a9b0
Jesse <jnavarro30@live.com>
2026-05-02 15:38:46 -0700

    checkpoint

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="container">
+    <button
+      class="hamburger left"
+      :aria-expanded="showDrawer"
+      @click="showDrawer = !showDrawer"
+      title="Toggle new bug"
+    >
+      &#9776;
+    </button>
     <div class="header">
-      <button
-        class="hamburger left"
-        :aria-expanded="showDrawer"
-        @click="showDrawer = !showDrawer"
-        title="Toggle new bug"
-      >
-        &#9776;
-      </button>
       <h1>Simple Bug Tracker</h1>
     </div>
 
     <div>
       <div class="left-drawer" :class="{ open: showDrawer }">
         <div style="padding: 12px">
-          <h3 style="margin: 0 0 8px 0">New Bug</h3>
           <BugForm @save="create" @cancel="showDrawer = false" />
         </div>
       </div>
