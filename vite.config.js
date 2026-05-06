d8390e5a5632d1b092a5fc9f61999f0ab84e4e4a
Jesse <jnavarro30@live.com>
2026-05-06 13:58:10 -0700

    build using manual chunks

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   base: "/bug-tracker/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "pdf-libs": ["jspdf", "html2canvas"],
+        },
+      },
+    },
+  },
 });
